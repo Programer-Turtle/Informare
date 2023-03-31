8cc8fed5485ab9aae2113c6b9cafaf2660cf3125
Turtle <125102455+Programer-Turtle@users.noreply.github.com>
2023-03-31 19:44:53 -0400

    Update inform.js

--- scripts/inform.js
+++ scripts/inform.js
@@ -1,5 +1,5 @@
-let menu = ["Beef Nachos", "Biscuits & Gravy", "Chicken Patty", "Saturday", "Sunday", "Hamb & Cheese", "Papa J Pizza", "Speghetti & Meatballs", "Chicken & Noodles", "Chicken Strips", "Saturday", "Sunday", "Mac & Cheese", "Popcorn Chicken", "Pizza Burger", "Chicken Leg", "Calzone", "Saturday", "Sunday", "Break", "Break", "Break", "Break", "Break", "Saturday", "Sunday", "Chicken Alfredo", "Papa J Pizza", "Buffalo Dip", "Hamb & Cheese", "Calzone"];
-let grade = [31, 45, 51, "N/A", "N/A", 15, 45 ,52, 25, 44, "N/A", "N/A", 38, 26, 43, 35, 22, "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", 48, 45, 0, 15, 22];
+let menu = ["Unavaiable Pls wait:)", "Biscuits & Gravy", "Chicken Patty", "Saturday", "Sunday", "Hamb & Cheese", "Papa J Pizza", "Speghetti & Meatballs", "Chicken & Noodles", "Chicken Strips", "Saturday", "Sunday", "Mac & Cheese", "Popcorn Chicken", "Pizza Burger", "Chicken Leg", "Calzone", "Saturday", "Sunday", "Break", "Break", "Break", "Break", "Break", "Saturday", "Sunday", "Chicken Alfredo", "Papa J Pizza", "Buffalo Dip", "Hamb & Cheese", "Calzone"];
+let grade = ["N/A", 45, 51, "N/A", "N/A", 15, 45 ,52, 25, 44, "N/A", "N/A", 38, 26, 43, 35, 22, "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", 48, 45, 0, 15, 22];
 var date = new Date();
 var day = date.getDate() - 1;
 let finalgrade = grade[day];
