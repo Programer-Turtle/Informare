a19c03ed7f0cf07e87cf39d64789a3d0b0594656
Turtle <125102455+Programer-Turtle@users.noreply.github.com>
2023-03-31 23:42:19 -0400

    Add files via upload

---
+++ scripts/classes.js
@@ -0,0 +1,30 @@
+var privatecounter = 1; // Declare the variable outside the function
+
+function addgoal() {
+  // Get input
+  var goal = document.getElementById("goal_input").value;
+  console.log(goal);
+
+  // Uploads cookie
+  document.cookie = `${privatecounter}=${goal}`;
+  privatecounter++;
+}
+var counter = 1;
+while (document.cookie.split(";")[counter] !== undefined)
+{
+    var newPara = document.createElement("p");
+
+    // Add some text to the paragraph element
+    var paraText = document.createTextNode(document.cookie.split(";")[counter].split("=")[1]);
+    newPara.appendChild(paraText);
+
+    // Get the div element to add the new paragraph to
+    var myDiv = document.getElementById("list");
+    console.log(myDiv);
+
+    // Add the new paragraph element to the div element
+    myDiv.appendChild(newPara);
+    console.log(newPara);
+
+    counter++;
+}
