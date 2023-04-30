16fbf314e625c1274c50828a5b46491566f3a6cb
Turtle <125102455+Programer-Turtle@users.noreply.github.com>
2023-04-30 12:00:59 -0400

    Update inform.js

--- scripts/inform.js
+++ scripts/inform.js
@@ -1,5 +1,5 @@
-let menu = ["Saturday", "Sunday", "Popcorn Chicken", "Chicken & Dumplings", "Loaded Potato Skins", "Pepperoni Calzone", "E Learning", "Saturday", "Sunday", "Hamb/Cheese", "Pizza", "Mozzarella Sticks", "Crispy Chicken Leg", "Chicken Patty", "Saturday", "Sunday", "Loaded Fries", "Pancakes/Syrup", "lasagna", "Popcorn Chicken", "Quesadilla", "Saturday", "Sunday", "Pepperoni Calzone", "Papa John's Pizza", "Corn Dog", "Popcorn Chicken", "Hamb/Cheese"];
-let grade = ["N/A", "N/A", 44.5, 43, 52, 17, "Have a good day", "N/A", "N/A", 16, 45, 52, 34, 60, "N/A", "N/A", 52, 60, 46, 44.5, 39, "N/A", "N/A", 17, 44, 32, 44.5, 16];
+let menu = ["Menu not availiable get mad at the school"];
+let grade = ["N/A"];
 var date = new Date();
 var day = date.getDate() - 1;
 let finalgrade = grade[day];
