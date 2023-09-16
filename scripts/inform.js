17da8b4be47405fd9a640d6cb6f3ed28b1976acc
Karson Ulerick <karsonulerick@gmail.com>
2023-09-16 12:48:24 -0400

    major Update

--- scripts/inform.js
+++ scripts/inform.js
@@ -15,6 +15,42 @@ if (finalgrade == 60)
     document.getElementById("grade").style.color = "yellow";
 }
 
+if (finalgrade <= 59 & finalgrade >= 51)//completed
+{
+    document.getElementById("grade").style.textShadow = "0 0 10px rgb(0, 255, 0), 0 0 20px rgb(0, 245, 0), 0 0 30px rgb(0, 155, 32)";
+    document.getElementById("grade").style.color = "rgb(0, 255, 0)";
+}
+
+if (finalgrade <= 50 & finalgrade >= 41)//completd
+{
+    document.getElementById("grade").style.textShadow = "0 0 10px rgb(145, 233, 0), 0 0 20px rgb(145, 223, 0), 0 0 30px rgb(145, 133, 32)";
+    document.getElementById("grade").style.color = "rgb(145, 233, 0)";
+}
+
+if (finalgrade <= 40 & finalgrade >= 31)//completed
+{
+    document.getElementById("grade").style.textShadow = "0 0 10px rgb(196, 186, 0), 0 0 20px rgb(196, 176, 0), 0 0 30px rgb(196, 86, 32)";
+    document.getElementById("grade").style.color = "rgb(196, 186, 0)";
+}
+
+if (finalgrade <= 30 & finalgrade >= 21) //completed
+{
+    document.getElementById("grade").style.textShadow = "0 0 10px rgb(229, 145, 0), 0 0 20px rgb(229, 135, 0), 0 0 30px rgb(229, 45, 32)";
+    document.getElementById("grade").style.color = "rgb(229, 145, 0)";
+}
+
+if (finalgrade <= 20 & finalgrade >= 11)//completed
+{
+    document.getElementById("grade").style.textShadow = "0 0 10px rgb(249, 96, 0), 0 0 20px rgb(249, 86, 0), 0 0 30px rgb(249, 6, 32)";// 1:same color; 2:same color, -10, samecolor; 3:same color, -90, +32;
+    document.getElementById("grade").style.color = "rgb(249, 96, 0)";
+}
+
+if (finalgrade <= 10 & finalgrade >= 0)
+{
+    document.getElementById("grade").style.textShadow = "0 0 10px rgb(255, 0, 0), 0 0 20px rgb(255, 10, 0), 0 0 30px rgb(255, 100, 32)";
+    document.getElementById("grade").style.color = "rgb(255, 0, 0)";
+}
+
 document.getElementById("Item").innerHTML = menu[day];
 document.getElementById("grade").innerHTML = finalgrade + "/60";
 
