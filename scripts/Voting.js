d7f3f8bed53c35f9111cabe41aac4577193c8869
Karson Ulerick <karsonulerick@gmail.com>
2023-10-23 23:59:19 -0400

    VotingUpdate

---
+++ scripts/Voting.js
@@ -0,0 +1,24 @@
+let closed = false;
+
+if (closed == true)
+{
+    if (localStorage.getItem("Submitted") != null)
+    {
+        localStorage.removeItem("Submitted")
+    }
+    document.getElementById('Form').style.display = "none";
+    document.getElementById('ClosedPage').style.display = "block";
+}
+
+if (localStorage.getItem("Submitted") != null)
+{
+    document.getElementById('Form').style.display = "none";
+    document.getElementById('SubmittedPage').style.display = "block";
+}
+
+document.getElementById('Form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    document.getElementById('Form').style.display = "none";
+    document.getElementById('SubmittedPage').style.display = "block";
+    localStorage.setItem("Submitted", "True")
+});
