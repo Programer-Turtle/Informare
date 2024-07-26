66ed81806b99bdf641b4ac40a037b433ae29603e
Informare <karsonulerick@gmail.com>
2024-07-26 18:15:54 -0400

    Added News and Music 2.8.9.9(Beta)

--- scripts/Music.js
+++ scripts/Music.js
@@ -1,49 +1,65 @@
-// let audioTrack = document.createElement("audio")
-// let htmlListen = document.getElementsByTagName("html")
-// htmlListen = htmlListen[0]
-// audioTrack.loop = true
+let audioTrack = document.createElement("audio")
+let htmlListen = document.getElementsByTagName("html")
+htmlListen = htmlListen[0]
+audioTrack.loop = true
 
-// function GetPage()
-// {
-//     var path = window.location.pathname;
-//     var page = path.split("/").pop();
-//     return page;
-// }
+function GetPage()
+{
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    return page;
+}
 
-// function PlayAudio(File)
-// {
-//     audioTrack.setAttribute("src", `Songs/${File}`)
-//     audioTrack.play()
-// }
+function PlayAudio(File)
+{
+    audioTrack.setAttribute("src", `Songs/${File}`)
+    audioTrack.play()
+}
 
-// function OnMove(event)
-// {
-//     let page = GetPage()
-//     if (page == "Posting.html" || page == "Posting")
-//     {
-//         PlayAudio("AHappyTune.mp3")
-//     }
-//     else if (page == "Homework.html" || page == "Homework")
-//     {
-//         PlayAudio("WorkinTime.mp3")
-//     }
-//     else if (page == "Home.html" || page == "Home")
-//     {
-//         PlayAudio("ToBegin.mp3")
-//     }
-//     else if (page == "Policy.html" || page == "Policy")
-//     {
-//         PlayAudio("FrameandPlay.mp3")
-//     }
-//     else if (page == "MessageOfTheDay.html" || page == "MessageOfTheDay")
-//     {
-//         PlayAudio("AChillExploration.mp3")
-//     }
-//     else
-//     {
-//         PlayAudio("LoginScreen.mp3")
-//     }
-//     htmlListen.removeEventListener("click", OnMove)
-// }
+function OnMove(event)
+{
+    let page = GetPage()
+    if (page == "News.html" || page == "News")
+    {
+        PlayAudio("AHappyTune.mp3")
+    }
+    else if (page == "Homework.html" || page == "Homework")
+    {
+        PlayAudio("WorkinTime.mp3")
+    }
+    else if (page == "Home.html" || page == "Home")
+    {
+        PlayAudio("ToBegin.mp3")
+    }
+    else if (page == "Policy.html" || page == "Policy")
+    {
+        PlayAudio("FrameandPlay.mp3")
+    }
+    else if (page == "MessageOfTheDay.html" || page == "MessageOfTheDay")
+    {
+        PlayAudio("AChillExploration.mp3")
+    }
+    else if (page == "Moderation.html" || page == "Moderation")
+    {
+        PlayAudio("Storm.mp3")
+    }
+    else if (page == "updates.html" || page == "updates")
+    {
+        PlayAudio("InfosPace.mp3")
+    }
+    else if (page == "Voting.html" || page == "Voting")
+    {
+        PlayAudio("Tons.mp3")
+    }
+    else if (page == "Ban.html" || page == "Ban")
+    {
+        PlayAudio("Banned.mp3")
+    }
+    else
+    {
+        PlayAudio("LoginScreen.mp3")
+    }
+    htmlListen.removeEventListener("click", OnMove)
+}
 
-// htmlListen.addEventListener("click", OnMove)
+htmlListen.addEventListener("click", OnMove)
---
+++ scripts/News.js
@@ -0,0 +1,208 @@
+async function CheckIfWriter() {
+    let email = localStorage.getItem("username");
+    let StoredToken = localStorage.getItem("token");
+
+    if (!email || !StoredToken) {
+        return false;
+    }
+
+    try {
+        const response = await fetch('https://informare-web-server-karsonoculus.replit.app/CheckForPermission', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: email,
+                token: StoredToken,
+                permission: "News-Writer"
+            })
+        });
+
+        if (!response.ok) {
+            let errorData;
+            try {
+                errorData = await response.json();
+            } catch (parseError) {
+                // If parsing the error JSON fails, use the raw text as the error message
+                throw new Error(response || 'Server responded with an error');
+            }
+            
+            throw new Error(errorData.error);
+        }
+
+        const data = await response.json();
+        HidePopUp("LoadAnimation")
+        return data;
+
+    } catch (error) {
+        console.error(error)
+        return false;
+    }
+}
+
+async function GetNewsData()
+{
+    let email = localStorage.getItem("username");
+    let StoredToken = localStorage.getItem("token");
+    console.log(email)
+
+    if (!email || !StoredToken) {
+        return "<p style+'color:red;'>Error Occured</p>";
+    }
+
+    try {
+        const response = await fetch('https://informare-web-server-karsonoculus.replit.app/GetNews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: email,
+                token: StoredToken
+            })
+        });
+
+        if (!response.ok) {
+            let errorData;
+            try {
+                errorData = await response.json();
+            } catch (parseError) {
+                // If parsing the error JSON fails, use the raw text as the error message
+                throw new Error(response.statusText || 'Server responded with an error');
+            }
+            
+            throw new Error(errorData.error);
+        }
+
+        const data = await response.json();
+        HidePopUp("LoadAnimation2")
+        console.log(data)
+        return data.Data;
+
+    } catch (error) {
+        console.error(error)
+        return false;
+    }
+}
+
+async function SetNewsData()
+{
+    console.log("cheese")
+    let email = localStorage.getItem("username");
+    let StoredToken = localStorage.getItem("token");
+    let DataToPost = {Data: (document.getElementById("NewsEditInput").value)}
+
+    if (!email || !StoredToken) {
+        return false;
+    }
+    try{
+        const response = await fetch('https://informare-web-server-karsonoculus.replit.app/PostNews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: email,
+                token: StoredToken,
+                NewsData:DataToPost
+            })
+        });
+
+        if (!response.ok) {
+            let errorData;
+            try {
+                errorData = await response.json();
+            } catch (parseError) {
+                // If parsing the error JSON fails, use the raw text as the error message
+                throw new Error(response.statusText || 'Server responded with an error');
+            }
+            
+            throw new Error(errorData.error);
+        }
+        else{
+            console.log(response)
+            location.reload()
+        }
+    }
+    catch (error){
+        ChangeInnerText("ErrorText", error)
+        ShowPopUp("ErrorText")
+        return false;
+    }
+}
+
+async function GetUIData()
+{
+    const IfWriter = await CheckIfWriter()
+    if(IfWriter == true)
+    {
+        ShowPopUp("EditButton", "block")    
+    }
+    const CurrentNews = await GetNewsData()
+    ChangeInnerHTML("NewsText", CurrentNews)
+}
+
+//Text Area
+let textarea = document.getElementById("NewsEditInput")
+
+function InsertText(text)
+{
+    const position = textarea.selectionStart;
+    const before = textarea.value.substring(0,position)
+    const after = textarea.value.substring(position, textarea.value.length)
+    textarea.value = before + text + after
+    console.log(before + text + after)
+}
+
+function AddText()
+{
+    InsertText('<p style="color: white; font-size: 45px;">Put Text Here</p>')
+}
+
+function AddInnerText()
+{
+    InsertText('<span style="color: white; font-size: 45px;">Put Text Here</span>')
+}
+
+function AddImage()
+{
+    InsertText('<img width="" src="">')
+}
+
+function AddLink()
+{
+    InsertText('<a href="LinkHere"<p>Text Here</p></a>')
+}
+
+function AddHeading()
+{
+    InsertText('<h1 style="color:white;">Text Here</h1>')
+}
+
+function AddLeft()
+{
+    InsertText('<div class="left">Content Here</div>')
+}
+
+function AddCenter()
+{
+    InsertText('<div class="center">Content Here</div>')
+}
+
+function AddRight()
+{
+    InsertText('<div class="right">Content Here</div>')
+}
+
+textarea.addEventListener('input', (event) =>
+    {
+        textarea.style.height = ''
+        textarea.style.height = textarea.scrollHeight + 'px'
+    })
+
+// textarea.addEventListener('pointermove', (event) =>{
+//     position = textarea.selectionStart;
+// })
+
+GetUIData()
